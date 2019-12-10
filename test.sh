5b8eae9c0c40509e9e72bc1f69cc42e86ac629a7
Hetsh <git@hetsh.de>
2019-12-10 15:13:22 +0100

    Build image in project directory

--- test.sh
+++ test.sh
@@ -9,12 +9,10 @@ then
     exit 1
 fi
 
-TMP_DIR=$(mktemp -d /tmp/docker-TMP-XXXXXXXXXX)
+TMP_DIR=$(mktemp -d /tmp/mcserver-XXXXXXXXXX)
 trap "rm -rf $TMP_DIR" EXIT
-cp Dockerfile "$TMP_DIR"
 echo "eula=true" > "$TMP_DIR/eula.txt"
 chown -R 1357:1357 "$TMP_DIR"
 
-cd "$TMP_DIR"
 docker build --tag "mcserver" .
 docker run --rm --interactive --name mcserver --publish 25565:25565 --mount type=bind,source="$TMP_DIR",target=/mcserver mcserver
