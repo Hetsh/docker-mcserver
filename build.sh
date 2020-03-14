188b31b280636fd02e545da3c1ce9df8da140a64
Hetsh <git@hetsh.de>
2020-03-14 20:42:48 +0100

    Fix typos, sync time during test

--- build.sh
+++ build.sh
@@ -11,11 +11,11 @@ cd "$CWD"
 # Load helpful functions
 source libs/common.sh
 
-# Check acces do docker daemon
+# Check access to docker daemon
 assert_dependency "docker"
 if ! docker version &> /dev/null; then
-    echo "Docker daemon is not running or you have unsufficient permissions!"
-    exit -1
+	echo "Docker daemon is not running or you have unsufficient permissions!"
+	exit -1
 fi
 
 # Build the image
@@ -41,6 +41,7 @@ if confirm_action "Test image?"; then
 	--publish 25565:25565/udp \
 	--publish 25575:25575/tcp \
 	--mount type=bind,source="$TMP_DIR",target="$DATA_DIR" \
+	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 	--name "$APP_NAME" \
 	"$APP_NAME"
 fi
