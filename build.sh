d84766b211038b5b460986fddd2a1fc6e6c1e904
Hetsh <git@hetsh.de>
2020-11-16 11:56:54 +0100

    Remove non default test options

--- build.sh
+++ build.sh
@@ -27,17 +27,6 @@ docker build --tag "$IMG_NAME:latest" --tag "$IMG_NAME:$_NEXT_VERSION" .
 case "${1-}" in
 	# Test with default configuration
 	"--test")
-		# Set up temporary directory
-		TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
-		add_cleanup "rm -rf $TMP_DIR"
-		echo "eula=true" > "$TMP_DIR/eula.txt"
-
-		# Apply permissions, UID matches process user
-		extract_var APP_UID "./Dockerfile" "\K\d+"
-		chown -R "$APP_UID":"$APP_UID" "$TMP_DIR"
-
-		# Start the test
-		extract_var DATA_DIR "./Dockerfile" "\"\K[^\"]+"
 		docker run \
 		--rm \
 		--tty \
@@ -45,7 +34,6 @@ case "${1-}" in
 		--publish 25565:25565/tcp \
 		--publish 25565:25565/udp \
 		--publish 25575:25575/tcp \
-		--mount type=bind,source="$TMP_DIR",target="$DATA_DIR" \
 		--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 		--name "$APP_NAME" \
 		"$IMG_NAME"
