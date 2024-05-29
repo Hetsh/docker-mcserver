fab06af65154066a2dccc87a9c76faacd5e8d6eb
Hetsh <git@hetsh.de>
2024-05-29 16:59:39 +0200

    update.sh: don't use alias for "test"

--- update.sh
+++ update.sh
@@ -23,12 +23,12 @@ update_image "amd64/alpine" "Alpine Linux" "false" "\d{8}"
 NAME="MC Server"
 CURRENT_MC_VERSION="${_CURRENT_VERSION%-*}"
 NEW_MC_VERSION=$(curl --silent --location "https://launchermeta.mojang.com/mc/game/version_manifest.json" | jq -r ".latest.release")
-if [ -z "$CURRENT_MC_VERSION" ] || [ -z "$NEW_MC_VERSION" ]; then
+if test -z "$CURRENT_MC_VERSION" || test -z "$NEW_MC_VERSION"; then
 	echo -e "\e[31mFailed to get $NAME version!\e[0m"
-elif [ "$CURRENT_MC_VERSION" != "$NEW_MC_VERSION" ]; then
+elif test "$CURRENT_MC_VERSION" != "$NEW_MC_VERSION"; then
 	METADATA_URL=$(curl --silent --location "https://launchermeta.mojang.com/mc/game/version_manifest.json" | jq -r ".versions[] | select(.id==\"$NEW_MC_VERSION\") | .url")
 	DOWNLOAD_URL=$(curl --silent --location "$METADATA_URL" | jq -r ".downloads.server.url")
-	if [ -z "$DOWNLOAD_URL" ]; then
+	if test -z "$DOWNLOAD_URL"; then
 		echo -e "\e[31mFailed to get $NAME download url!\e[0m"
 	else
 		prepare_update "BIN_URL" "$NAME" "$CURRENT_MC_VERSION" "$NEW_MC_VERSION" "\".*\"" "\"$DOWNLOAD_URL\""
@@ -45,10 +45,10 @@ if ! updates_available; then
 fi
 
 # Perform modifications
-if [ "${1-}" = "--noconfirm" ] || confirm_action "Save changes?"; then
+if test "${1-}" = "--noconfirm" || confirm_action "Save changes?"; then
 	save_changes
 
-	if [ "${1-}" = "--noconfirm" ] || confirm_action "Commit changes?"; then
+	if test "${1-}" = "--noconfirm" || confirm_action "Commit changes?"; then
 		commit_changes
 	fi
 fi
