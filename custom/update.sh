cfaff4a360269421f7a2e09beb8c1ffb7895579a
Hetsh <git@hetsh.de>
2026-02-03 09:43:02 +0100

    update.sh: reduce requests to mojang.com

--- custom/update.sh
+++ custom/update.sh
@@ -12,8 +12,9 @@ function check_for_updates {
 
 	URL_ID="BIN_URL"
 	CURRENT_DOWNLOAD_URL=$(grep --only-matching --perl-regexp "(?<=$URL_ID=\")[^\"]+" "Dockerfile")
-	NEW_MC_VERSION=$(curl_request "https://launchermeta.mojang.com/mc/game/version_manifest.json" | jq -r ".latest.release")
-	METADATA_URL=$(curl_request "https://launchermeta.mojang.com/mc/game/version_manifest.json" | jq -r ".versions[] | select(.id==\"$NEW_MC_VERSION\") | .url")
+	MC_MANIFEST=$(curl_request "https://launchermeta.mojang.com/mc/game/version_manifest.json")
+	NEW_MC_VERSION=$(jq -r ".latest.release" <<< "$MC_MANIFEST")
+	METADATA_URL=$(jq -r ".versions[] | select(.id==\"$NEW_MC_VERSION\") | .url" <<< "$MC_MANIFEST")
 	NEW_DOWNLOAD_URL=$(curl_request "$METADATA_URL" | jq -r ".downloads.server.url")
 	process_update "$URL_ID" "\"$CURRENT_DOWNLOAD_URL\"" "\"$NEW_DOWNLOAD_URL\"" "MC Server" "${GIT_VERSION%-*}" "$NEW_MC_VERSION"
 
